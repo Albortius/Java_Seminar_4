7722c7c3336b545a5c8f13eff67e35a81c633324
Denis <elspy@mail.ru>
2022-12-12 23:18:20 +0300

    Завершен процесс решения ДЗ по семинару

--- task_1.java
+++ task_1.java
@@ -1,5 +1,35 @@
+import java.util.List;
+import java.util.LinkedList;
+import java.util.Scanner;
+import java.util.Random;
+
 public class task_1 {
     public static void main(String[] args) {
-        System.out.println("IT IS OK");
-    }
+        Scanner in = new Scanner(System.in);
+        System.out.print("Введите размер массива: ");
+        int arrSize = in.nextInt();
+        System.out.print("Максимальное значение элементов:");
+        int maxValue = in.nextInt();
+        LinkedList oldList = new LinkedList<Integer>();
+        Random randNumber = new Random();
+        System.out.println("Исходный массив: ");
+        for (int i = 0; i < arrSize; i++){
+            int elem = randNumber.nextInt(1, maxValue);
+            oldList.add(elem);
+            System.out.print(elem+" ");
+        }
+        int step    = oldList.size() / 2;
+        if (oldList.size() % 2 == 0){
+            step = (oldList.size() / 2) - 1;
+        }
+        int last    = oldList.size() - 1;
+        LinkedList newList = new LinkedList<Integer>();
+        System.out.println("\nПеревернутый массив: ");
+        for (int i = 0; i < oldList.size(); i++){
+            int elem = (Integer) oldList.get(last);
+            newList.add(elem);
+            System.out.print(elem+" ");
+            last--;
+        }
+    }   
 }
--- task_2.java
+++ task_2.java
@@ -1,5 +1,41 @@
+import java.util.List;
+import java.util.Queue;
+import java.util.LinkedList;
+import java.util.Scanner;
+import java.util.Random;
+import java.util.Collections;
+
 public class task_2 {
     public static void main(String[] args) {
-        System.out.println("OK");
+        Scanner in = new Scanner(System.in);
+        System.out.print("Введите размер массива: ");
+        int arrSize = in.nextInt();
+        System.out.print("Максимальное значение элементов: ");
+        int maxValue = in.nextInt();
+        Queue <Integer> oldList = new LinkedList(){};
+        Random randNumber = new Random();
+        System.out.print("Исходный массив: ");
+        for (int i = 0; i < arrSize; i++){
+            int elem = randNumber.nextInt(1, maxValue);
+            oldList.add(elem);
+            System.out.print(elem+" ");
+        }
+        System.out.print("\nПомещаем в конец очереди: ");
+        int lastChar = in.nextInt();
+        oldList.add(lastChar); // Добавляем в конец
+        System.out.println("Массив после дополнения: ");
+        for (int elem : oldList){
+            System.out.print(elem+" ");
+        }
+        int delFirst = oldList.remove(); //
+        System.out.println("\nУдаленный первый элемент: "+delFirst);
+        for (int elem : oldList){
+            System.out.print(elem+" ");
+        }
+        int getFirst = oldList.element();
+        System.out.println("\nПолучаем первый элемент: "+getFirst);
+        for (int elem : oldList){
+            System.out.print(elem+" ");
+        }
     }
 }
